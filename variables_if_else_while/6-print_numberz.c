e4a6d352c4627a302f17455e391d83eac58dc6d9
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-22 09:04:22 +0000

    creating file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57 ; i++)
+{
+putchar((char)i);
+}
+printf("\n");
+return (0);
+}
