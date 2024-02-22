790fd06d84d4fbb26bf2e372bd8c2f4091b42af8
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-22 08:32:53 +0000

    creating file 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch == 'e' || ch == 'q')
+continue;
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
