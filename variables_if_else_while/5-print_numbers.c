5e5f6f63721114564bc693081db2f7e8d9d9f94c
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-22 08:45:52 +0000

    creating file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: o (success)
+ */
+int main(void)
+{
+int i;
+for ( i = 0 ; i <= 9 ; i++)
+{
+printf( "%d" , i);
+}
+printf( "\n" );
+return (0);
+}
