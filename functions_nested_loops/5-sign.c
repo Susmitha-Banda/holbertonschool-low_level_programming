75ef7e966e44629f62cae5ea538e2736d069f4c9
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-23 10:45:43 +0000

    creating file 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_sign - check the sign of a number
+ * @c: the character to be checked
+ *
+ * Return: 1 if n is greater than 0, 0 if n is equal to 0, else -1
+ */
+int print_sign(int n);
+{
+if ( n > 0 )
+return (1);
+else if ( n == 0)
+return (0)
+else
+return (-1);
+}
