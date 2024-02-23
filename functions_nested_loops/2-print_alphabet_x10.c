7eec904edb7ab9a4eb23805576b55e232aefde79
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-23 10:26:24 +0000

    creating file 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet in lowercase
- *
+ * print_alphabet_x10 - print alphabet 10 times in lowercase
+ * Description: print alpahebet 10 times in lowercase
  * Return: void
  */
 void print_alphabet_x10(void)
