2eda6a7fb294d4ef330cc087a70d82c78ee5f387
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-23 23:10:47 +0000

    creating file 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+\**
+*_abs-prints the absolute value of an integer
+*@num-the number to be processed
+*
+*
+*return:the abosolute value of an integer
+*/
+int _abs(int i)
+{
+if ( i >= 0 )
+return ( i );
+else
+return ( -i );
+}
