7f87242d55f1a0514b0abbe0edeb6803669ff465
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-23 10:09:03 +0000

    creating file 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+char ch;
+for(i=o ; i<=9 ; i++)
+{
+for(ch = 'a'; ch <= 'z'; ++ch)
+{
+_putchar(ch);
+}
+}
+_putchar('\n');
+}
