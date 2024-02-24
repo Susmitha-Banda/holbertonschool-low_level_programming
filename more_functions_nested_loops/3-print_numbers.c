999706202ea282df4db6706679053d82bfcc93aa
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 05:06:07 +0000

    creating file 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
+ *print_numbers - print numbers from 0 to 9
  *
  *
  *
  *
- *
- *
+ *return: void
  */
 void print_numbers(void)
 {
-for (i == 0 ; i <= 9 ; i++)
+int i:
+for (i = 0 ; i <= 9 ; i++)
 {
 _putchar('0' + i);
 }
