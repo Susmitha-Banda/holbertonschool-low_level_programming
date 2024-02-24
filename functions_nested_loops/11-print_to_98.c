3a550bdc31b0fdbb755705976f94e77328cc2fec
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 03:09:35 +0000

    creating file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,14 @@
 void print_to_98(int n)
 {
 int i;
-for ( i = n ; i <= 98 ; i++)
+for (i = n ; i <= 98 ; i++)
 {
 _putchar('0' +  i);
+if (i != 98)
+{
 _putchar(',');
 _putchar(' ');
+}
 }
 _putchar('\n');
 }
