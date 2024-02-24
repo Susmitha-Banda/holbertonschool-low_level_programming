a7b84e69d6275b4c133b28327749c7afe82d4f33
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 06:20:06 +0000

    creating file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagonal - Drawsa diagonal line on terminal
+ * @n: number of lines
+ */
+void print_diagonal(int n)
+{
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j < i ; j++)
+{
+_putchar(' ');
+}
+_putchar('\');
+_putchar('\n');
+}
+}
