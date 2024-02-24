f598bb4db9567170b36f1858e257d241d6964fb4
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 05:43:47 +0000

    creating file 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@
 void more_numbers(void)
 {
 int i, j;
-for (i = 1 ; i <= 10 ; i++)
+for (i = 1; i <= 10; i++)
 {
-for (j = 0 ; j <= 14 ; j++)
+for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
-_putchar('0' + i);
+_putchar('0' + 1);
 _putchar('0' + (j % 10));
 }
 _putchar('\n');
