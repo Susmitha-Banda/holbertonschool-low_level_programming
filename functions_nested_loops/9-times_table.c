3c288d3c18fb409e136ca091a46044b350d57538
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 00:46:03 +0000

    creating file 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,12 +5,15 @@
  */
 void times_table(void)
 {
-int i , j;
+int i , j , a;
 for ( i = 0 ; i <= 9 ; i++)
 {
 for ( j = 0 ; j <= 9 ; j++)
 {
-_putchar('0' + (i * j));
+int a = i * j;
+_putchar('0' + a);
+_putchar('0' + (a / 10));
+_putchar('0' + (a % 10));
 _putchar(',');
 _putchar(' ');
 }
