c2b02e69d9a4acc42f02bb2630ad6c219643ebc6
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-24 04:16:53 +0000

    creating file 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.c"
+\**
+ *check for a uppercase character
+ *_isupper: check if it is uppercase
+ *
+ *
+ *Return: 1 if c is uppercase else 0
+ *
+ *\
+int _isupper(int c);
+{
+char ch;
+if (ch == 'A' ; ch <= 'Z' ; ch++)
+{
+_putchar('ch');
+else if (ch == c)
+return (1);
+else
+return (0);
+}
+_putchar('\n');
+}
