b57377c31e1fd07866099bf238d812096574ed6a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-28 11:13:18 +0000

    creating file 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * print_array - print n elements of an array of integers
+ * @a: array to be processed
+ * @n: number of elements of the array to be printed
  */
 void print_array(int *a, int n)
 {
