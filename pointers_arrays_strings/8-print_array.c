e0976bc76b5f883b8f2b406964f0f36dcb96a730
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-02-28 10:49:23 +0000

    creating file 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *
  *
@@ -9,5 +10,5 @@ void print_array(int *a, int n)
 {
 int i;
 for (i = 0 ; i <= n ; i++)
-printf(" %d", *a[i] );
+printf(" %d", a[i] );
 }
