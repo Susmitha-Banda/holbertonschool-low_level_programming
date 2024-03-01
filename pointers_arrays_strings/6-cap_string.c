b70a327c09492843e40d7c167869af5827bb0855
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-01 08:40:22 +0000

    creating file new 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ y = 1;
 }
 else if (((str[x] >= 65 && str[x] <= 90) || (str[x] >= 48 && str[x] <= 57)) && y == 0) 
 y = 1;
-if (str[x] == 32 || str[x] == 10 || str[x] == 46)
+if (str[x] == 32 || str[x] == 10 || str[x] == 46 || str[x] == 9)
 y = 0;
 }
 return (str);
