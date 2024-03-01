11d411650f72600ccce5769eff775f84d77b16ca
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-01 06:38:33 +0000

    creating file 0 again

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,10 @@ char *_strcat(char *dest, char *src)
 {
 int i, j;
 i = 0;
-while (i = 0 ; dest[i] != '\0')
+while (dest[i] != '\0')
 i++;
 j = 0;
-while (j = 0 ; src[i] != '\0')
+while (src[i] != '\0')
 {
 dest[i] = src[j];
 j++;
