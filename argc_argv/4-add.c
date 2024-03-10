0e6d6564a65a901d6533936f197773ccdc0fb341
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 04:11:35 +0000

    creating file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  *main - prints the multipilcation of two numbers, followed by a new line
  *@argc: arguments count
@@ -11,10 +12,12 @@
  */
 int main(int argc, char *argv[])
 {
+char int_str[20];
 int i, sum = 0 ;
 for (i = 1 ; i < argc ; i++)
 {
-if (argv[i] != 0 && atoi(argv[i]) == 0)
+sprintf(int_str, "%d", atoi(argv[i]));	
+if (argv[i] != 0 &&  (atoi(argv[i]) == 0 || strlen(argv[i]) != strlen(int_str)))
 {
 printf("Error\n");
 return (1);
