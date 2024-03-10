49c61aedd5d50fa1237dfb4953e73a09730e80d0
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 01:35:40 +0000

    creating file 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints the multipilcation of two numbers, followed by a new line
+ *@argc: arguments count
+ *@argv: arguments array
+ *
+ *Description: prints the result of multiplication
+ *Return: 0 on sucess and if the program does not receive two arguments, print Error, followed by a new line, and return 1
+ */
+int main(int argc, char *argv[])
+{
+int i, j;
+i = j = 0;
+if (argc == 3)
+{
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", i * j);
+return (0);
+}
+else 
+printf("error\n");
+return (1);
+}
