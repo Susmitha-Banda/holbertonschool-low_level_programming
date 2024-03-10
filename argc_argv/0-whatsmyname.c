5e9f03931553cb9ffc6b6ab7260ce758bb7c6ae4
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 00:53:19 +0000

    creating file 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  *
  * Return: 0 on success
  */
-int main(int argc , char *argv[])
+int main(int argc ,char *argv[])
 {
 if (argc > 0)
-printf ("%s\n", argv[0]);
+printf("%s\n",argv[0]);
 return (0);
 }
