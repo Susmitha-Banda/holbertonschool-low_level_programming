93245eb1ca75bb758c8ca67aa41456c247b42b59
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 04:13:40 +0000

    creating file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,11 +13,12 @@
 int main(int argc, char *argv[])
 {
 char int_str[20];
-int i, sum = 0 ;
+int i, sum = 0;
 for (i = 1 ; i < argc ; i++)
 {
-sprintf(int_str, "%d", atoi(argv[i]));	
-if (argv[i] != 0 &&  (atoi(argv[i]) == 0 || strlen(argv[i]) != strlen(int_str)))
+sprintf(int_str, "%d", atoi(argv[i]));
+if (argv[i] != 0 &&
+(atoi(argv[i]) == 0 || strlen(argv[i]) != strlen(int_str)))
 {
 printf("Error\n");
 return (1);
