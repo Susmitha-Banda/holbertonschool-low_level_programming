77a8818c1ee4782bb6abbb4a7676fd4180c4c7e0
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 00:51:53 +0000

    creating file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - print name of the program, followed by a new name
+ *@argc: argument count
+ *@argv: argument array
+ *Description: prints the name of program
+ *
+ * Return: 0 on success
+ */
+int main(int argc , char *argv[])
+{
+if (argc > 0)
+printf ("%s\n", argv[0]);
+return (0);
+}
