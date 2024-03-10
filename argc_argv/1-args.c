37f31682a82c34325995dbc290f9ee791e757515
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 01:09:36 +0000

    creating file 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Description: prints the number of arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 printf ("%d\n", argc -1);
 return (0);
