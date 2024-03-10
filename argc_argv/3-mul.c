c015d2b0123829c5b5a7c30a536239ce8dbcdadc
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 01:39:09 +0000

    creating file 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -7,7 +7,8 @@
  *@argv: arguments array
  *
  *Description: prints the result of multiplication
- *Return: 0 on sucess and if the program does not receive two arguments, print Error, followed by a new line, and return 1
+ *Return: 0 on sucess and if the program
+ *does not receive two arguments, print Error, followed by a new line, and return 1
  */
 int main(int argc, char *argv[])
 {
@@ -17,10 +18,10 @@ if (argc == 3)
 {
 i = atoi(argv[1]);
 j = atoi(argv[2]);
-printf("%d\n", i * j);
+printf("%d\n", i *j);
 return (0);
 }
-else 
+else
 printf("Error\n");
 return (1);
 }
