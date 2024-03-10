fd98ed8a0fcdceb73800129d9a9c8da451efe25c
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-10 01:08:05 +0000

    creating file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the number of arguments, followed by new line
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Description: prints the number of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+printf ("%d\n", argc -1);
+return (0);
+}
