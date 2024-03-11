edfde5e64435859a4e70680e7034e66eac24e8fd
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-11 00:34:41 +0000

    creating file 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup() - returns a pointer to a new string
+ * which is a duplicate of the string str
+ *
+ *@str: string to copy
+ *
+ *Return: NULL if str=NULL, NULL if insifficient is available
+ *pointer to duplicate string
+ *
+ */
+char *_strdup(char *str)
+{
+char *pointer;
+unsigned int i, j;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++);
+i++;
+if (i < 1)
+return (NULL);
+pointer = malloc(len * sizeof(char));
+if (pointer == NULL)
+{
+free(pointer);
+return (NULL);
+}
+for (j = 0; j < i; j++)
+p[j] = str[j];
+p[j] = '\0';
+return (pointer);
+}
