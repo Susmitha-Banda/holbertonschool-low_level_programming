5ee380eff17b1948a16df25110d01dc00edec6b5
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-14 08:10:16 +0000

    creating file dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - struct for dog attributes
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
