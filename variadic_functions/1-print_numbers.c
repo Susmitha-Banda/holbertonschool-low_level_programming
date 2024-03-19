0c65550fa6e043cef825ef23176106e773a8de34
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-19 09:55:16 +0000

    creating file 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ va_list args;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-printf("%d\n", va_arg(args, int));
+printf("%d", va_arg(args, int));
 if (separator != NULL && i < n-1)
 {
 printf("%s", separator);
