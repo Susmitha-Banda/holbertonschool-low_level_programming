4c02017ff6bfce84f54851b1d565724573f4a8a3
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-19 09:32:37 +0000

    creating file 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -18,11 +18,11 @@ if (n == 0)
 {
 return (0);
 }
-va_start(args ,n);
+va_start(args, n);
 for (i = 0; i < n; i++)
 {
 sum += va_arg(args, int);
 }
-va_end (args);
-return sum;
+va_end(args);
+return (sum);
 }
