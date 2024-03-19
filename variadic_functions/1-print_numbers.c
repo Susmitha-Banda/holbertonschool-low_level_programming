de65d1f88438a4e6c6eb6649985c2b58ff93e450
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-19 09:56:46 +0000

    creating file 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- print_numbers - function that prints numbers
+ *print_numbers - function that prints numbers
  *
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function*
@@ -16,7 +16,7 @@ va_start(args, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(args, int));
-if (separator != NULL && i < n-1)
+if (separator != NULL && i < n - 1)
 {
 printf("%s", separator);
 }
