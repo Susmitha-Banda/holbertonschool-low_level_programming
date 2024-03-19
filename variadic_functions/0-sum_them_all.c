84d1e641e79c65204701b799b14ff3616d428e37
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-19 09:16:47 +0000

    creating file 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+/**
+ *sum_them_all - Finds sum of all its parameters
+ *
+ * @n: number of integers
+ *
+ * Return: sum of all its parameters
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{  
+if (n == 0)
+{
+return (0);
+}
+int sum = 0;
+va_list args;
+va_start(args ,n);
+for (unsigned int i = 0; i < n; i++)
+{
+sum += va_args(args , int);
+}
+va_end (args);
+return sum;
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct valid_types - struct prints data types
+ *
+ * @type: data type
+ * @p: pointer to function
+ *
+ */
+typedef struct valid_types
+{
+char type;
+void (*p)(va_list);
+} v_type;
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
