a035625e98305d9ab38bde4c9a7961ed81a6e931
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-19 09:28:33 +0000

    creating file 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,18 +10,18 @@
  *
  */
 int sum_them_all(const unsigned int n, ...)
-{  
+{
+int sum = 0;
+unsigned int i;
 if (n == 0)
 {
 return (0);
 }
-int sum = 0;
 va_list args;
 va_start(args ,n);
-unsigned int i;
 for (i = 0; i < n; i++)
 {
-sum += va_args(args , int);
+sum += va_arg(args , i);
 }
 va_end (args);
 return sum;
