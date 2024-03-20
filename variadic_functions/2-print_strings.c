82623336ace83af94f209a1acf4c0903fefc2008
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-03-20 08:13:13 +0000

    creating file 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,9 +1,10 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
+ * print_strings - prints a number of strings
  *
- *
- *
+ * @separator: string to be printed between the strings
+ * @n: number of strings
  *
  */
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -11,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list args;
 char *s;
 unsigned int i;
-va_start (args, n);
+va_start(args, n);
 for (i = 0; i < n; i++)
 {
 s = va_arg(args, char *);
