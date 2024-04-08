0503cf48b1fe0a298894986b21db7d3551e9646a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-04-08 07:11:38 +0000

    handling when lenght is 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 size_t i;
 for (i = 0; h; i++)
 {
-printf("[%d] %s\n", h->len, h->str == null ? 'nil' : h -> str);
+printf("[%d] %s\n", h->str ? h->len : 0, h->str ? h->str : "nil");
 h = h->next;
 }
 return (i);
