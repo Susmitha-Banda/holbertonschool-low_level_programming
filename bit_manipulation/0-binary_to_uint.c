0cb1f537b84d1d32788dafc528bf2dd6fc53e4ce
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-04-10 02:09:02 +0000

    changes to kernel style

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -24,17 +24,17 @@ unsigned int binary_to_uint(const char *b)
 unsigned int result = 0;
 if (b == NULL)
 {
-return(0);
+return (0);
 }
-while(*b != '\0')
+while (*b != '\0')
 {
 if (*b != '0' && *b != '1')
 {
-return(0);
+return (0);
 }
 result <<= 1;
 result += *b - '0';
 b++;
 }
-return result;
+return (result);
 }
